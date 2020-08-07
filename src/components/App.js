675adb4b8a71d8f44732ff1a01d7f0c86ba9bbb3
Alexey Grigoriev <st.graalex@gmail.com>
2020-08-07 19:56:28 +0300

    one more change

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Article from './Article'
+import ArticleList from './ArticleList'
 import articles from '../data'
 
 function App() {
     return (
         <div>
             <h1>App name</h1>
-            <Article article={articles[0]} foo="bar" flag={true}/>
+            <ArticleList articles={articles}/>
         </div>
     )
 } 
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Article(props) {
-    const {article} = props
-    const body = <section>{article.text}</section>
-
-    console.log('propy:', props)
+class Article extends Component {
+        state = {
+            isOpen: true
+        }
+render() {
+    const {article} = this.props
+    const body = this.state.isOpen && <section>{article.text}</section>
 
     return (
         <div>
+            <button onClick={this.handleClick}>close</button>
             <h2>{article.title}</h2>
             {body}
             <h3>creation date:{(new Date(article.date).toDateString())}</h3>
         </div>
     )
-} 
+}
+handleClick = () => {
+    this.setState({
+        isOpen: !this.state.isOpen
+    })
+}
+}
 
 export default Article
